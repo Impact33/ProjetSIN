ba24c49919dc7af0a1d094a28a796d623725e30e
Gael <gael.lemarechal@neuf.fr>
2020-03-26 19:29:36 +0100

    (feat):MAJ_CODE

--- PantiltProgramme.py
+++ PantiltProgramme.py
@@ -5,42 +5,41 @@ pantilthat.idle_timeout(0.5)
 
 # definir les angles
 def mvt_D():
-	pantilthat.pan(-45)
+	pantilthat.pan(-20)
 	
 def mvt_G():
-	pantilthat.pan(45)
+	pantilthat.pan(20)
 	
 def mvt_H():
-	pantilthat.tilt(-45)
+	pantilthat.tilt(-35)
 	
 def mvt_B():
-	pantilthat.tilt(45)
+	pantilthat.tilt(35)
 	
-
 # creation fenetre
-window = Tk()
+fenetre = Tk()
 
 # personnalisation fenetre
-window.title("Orientation camera")
-window.geometry("480x360")
-window.minsize(480, 360)
-window.config(background='#41B77F')
+fenetre.title("Orientation camera")
+fenetre.geometry("480x360")
+fenetre.minsize(480, 360)
+fenetre.config(background='#41B77F')
 
 # creation des frames
-frame1 = Frame(window, bg='#41B77F')
-frame2 = Frame(window, bg='#41B77F')
-frame3 = Frame(window, bg='#41B77F')
-frame4 = Frame(window, bg='#41B77F')
+frame1 = Frame(fenetre, bg='#41B77F')
+frame2 = Frame(fenetre, bg='#41B77F')
+frame3 = Frame(fenetre, bg='#41B77F')
+frame4 = Frame(fenetre, bg='#41B77F')
 
 # ajout de boutons
-mv_buttonD = Button(frame1, text="D", font=("Courrier", 25), bg='white', fg='#41B77F', command=mvt_D)
-mv_buttonD.pack()
-mv_buttonG = Button(frame2, text="G", font=("Courrier", 25), bg='white', fg='#41B77F', command=mvt_G)
-mv_buttonG.pack()
-mv_buttonH = Button(frame3, text="H", font=("Courrier", 25), bg='white', fg='#41B77F', command=mvt_H)
-mv_buttonH.pack()
-mv_buttonB = Button(frame4, text="B", font=("Courrier", 25), bg='white', fg='#41B77F', command=mvt_B)
-mv_buttonB.pack()
+mv_bouttonD = Button(frame1, text="D", font=("Courrier", 25), bg='white', fg='#41B77F', command=mvt_D)
+mv_bouttonD.pack()
+mv_bouttonG = Button(frame2, text="G", font=("Courrier", 25), bg='white', fg='#41B77F', command=mvt_G)
+mv_bouttonG.pack()
+mv_bouttonH = Button(frame3, text="H", font=("Courrier", 25), bg='white', fg='#41B77F', command=mvt_H)
+mv_bouttonH.pack()
+mv_bouttonB = Button(frame4, text="B", font=("Courrier", 25), bg='white', fg='#41B77F', command=mvt_B)
+mv_bouttonB.pack()
 
 # ajout des frames
 frame1.pack(side=RIGHT)
@@ -49,4 +48,4 @@ frame3.pack(side=TOP)
 frame4.pack(side=BOTTOM)
 
 # afficher fenetre
-window.mainloop()
+fenetre.mainloop()
